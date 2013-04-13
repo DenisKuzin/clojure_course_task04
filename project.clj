763e6ca6f5d95f98fb5b63e74316945b883db4a2
DenisKuzin <digiholiq@gmail.com>
2013-04-13 12:50:13 +0400

    Compojure add

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject clojure_course_task04 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main clojure-course-task04.core)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler clojure_course_task04.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
--- test/clojure_course_task04/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-course-task04.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-course-task04.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
