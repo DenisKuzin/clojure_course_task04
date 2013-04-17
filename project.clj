8777828ddd179a85dc99ddc0d492203581d56cd9
DenisKuzin <digiholiq@gmail.com>
2013-04-17 16:41:39 +0400

    Try msql

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [me.raynes/laser "1.1.1"]
-                 ;[criterium "0.3.1"]
                  [korma "0.3.0-RC5"]
-                 [org.postgresql/postgresql "9.2-1002-jdbc4"]]
+                 [mysql/mysql-connector-java "5.1.24"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler clojure_course_task04.handler/app}
   :profiles
--- src/clojure_course_task04/model.clj
+++ src/clojure_course_task04/model.clj
@@ -1,17 +1,30 @@
 (ns clojure_course_task04.model
   (:use [korma db core]))
 
-;(defdb clojure
-;  {:classname "org.postgresql.Driver"
-;   :subprotocol "postgresql"
-;   :subname "clojure"
-;   :user "postgres"
-;   :password "postgres"})
+;(defdb clojure (postgres
+;                {:db "clojure"
+;                 :user "postgres"
+;                 :password "postgres"}))
+(def env (into {} (System/getenv)))
+(def dbhost (get env "OPENSHIFT_MYSQL_DB_HOST"))
+(def dbport (get env "OPENSHIFT_MYSQL_DB_PORT"))
+(def dbuser (get env "OPENSHIFT_MYSQL_DB_USERNAME"))
+(def dbpass (get env "OPENSHIFT_MYSQL_DB_PASSWORD"))
+(def dburl (get env "OPENSHIFT_MYSQL_DB_URL"))
+;(def default-conn {:classname "com.mysql.jdbc.Driver"
+;                   :subprotocol "mysql"
+;                   :user dbuser
+;                   :password dbpass
+;                   :subname dburl;;(str "//" dbhost ":" dbport "/clojure?useUnicode=true&characterEncoding=utf8")
+;                   :delimiters "`"})
 
-(defdb clojure (postgres
+(defdb clojure (mysql
                 {:db "clojure"
-                 :user "postgres"
-                 :password "postgres"}))
+                 :host dbhost
+                 :port dbport
+                 :user dbuser
+                 :password dbpass
+                 }))
 
 (defentity problem)
 
@@ -19,7 +32,7 @@
   (has-one problem))
 
 (defn select-problems []
-  (select problem))
+   (select problem))
 
 (defn select-solutions []
   (select solution))
