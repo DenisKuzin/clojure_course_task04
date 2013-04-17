9f83cde065cd830c44d2ee9ec8be7fe1aeb71338
DenisKuzin <digiholiq@gmail.com>
2013-04-17 16:54:36 +0400

    Bugfixes

--- src/clojure_course_task04/handler.clj
+++ src/clojure_course_task04/handler.clj
@@ -1,20 +1,19 @@
 (ns clojure_course_task04.handler
   (:use compojure.core
-;        criterium.core
         clojure_course_task04.model
         clojure_course_task04.view)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.util.response :as resp]))
 
-(defn add-problem [problem]
-  (insert-problem problem)
-  (resp/redirect "/timeit"))
+(defn add-problem [name description]
+  (let [problem {:name name :description description}]
+    (insert-problem problem)
+    (resp/redirect "/timeit")))
 
 (defn add-solution [problem expression]
   (let [solution {:problem problem :expression expression 
                   :result (with-out-str (time (load-string expression)))}]
-                                         ;(with-out-str (benchmark (load-string expression)))}]
     (insert-solution solution)
     (resp/redirect "/timeit")))
 
@@ -25,7 +24,7 @@
   (GET "/timeit" [] (show-timeit (select-problems) (select-solutions)))
   (POST "/timeit" [problem expression] (add-solution problem expression))
   (GET "/timeit-add-problem" [] (show-add-problem))
-  (POST "/timeit-add-problem" [req] (add-problem (:params req)))
+  (POST "/timeit-add-problem" [name description] (add-problem name description))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- src/clojure_course_task04/model.clj
+++ src/clojure_course_task04/model.clj
@@ -1,23 +1,11 @@
 (ns clojure_course_task04.model
   (:use [korma db core]))
 
-;(defdb clojure (postgres
-;                {:db "clojure"
-;                 :user "postgres"
-;                 :password "postgres"}))
 (def env (into {} (System/getenv)))
 (def dbhost (get env "OPENSHIFT_MYSQL_DB_HOST"))
 (def dbport (get env "OPENSHIFT_MYSQL_DB_PORT"))
 (def dbuser (get env "OPENSHIFT_MYSQL_DB_USERNAME"))
 (def dbpass (get env "OPENSHIFT_MYSQL_DB_PASSWORD"))
-(def dburl (get env "OPENSHIFT_MYSQL_DB_URL"))
-;(def default-conn {:classname "com.mysql.jdbc.Driver"
-;                   :subprotocol "mysql"
-;                   :user dbuser
-;                   :password dbpass
-;                   :subname dburl;;(str "//" dbhost ":" dbport "/clojure?useUnicode=true&characterEncoding=utf8")
-;                   :delimiters "`"})
-
 (defdb clojure (mysql
                 {:db "clojure"
                  :host dbhost
